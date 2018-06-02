afdc3e311bd64eb4d029433d9d8b5c5bf6aeeef2
WSeegers <wseegers.mauws@gmail.com>
2018-06-02 18:32:04 +0200

    fread norm

--- f_io/f_readf.c
+++ f_io/f_readf.c
@@ -6,7 +6,7 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/30 09:01:06 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/02 18:20:21 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/02 18:31:45 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,22 @@
 #include "f_memory.h"
 #include "f_print.h"
 
-ssize_t	f_readf(char *ptr, t_file *file, size_t n)
+static int	check_buf(t_file *file)
+{
+	if (file->fpos == -1 || file->fpos == file->lread)
+	{
+		if (file->fpos > 0 && file->lread < file->cap)
+		{
+			file->fpos = -1;
+			return (1);
+		}
+		else if (!f_feedf(file))
+			return (1);
+	}
+	return (0);
+}
+
+ssize_t		f_readf(char *ptr, t_file *file, size_t n)
 {
 	size_t csize;
 	size_t total;
@@ -26,14 +41,8 @@ ssize_t	f_readf(char *ptr, t_file *file, size_t n)
 		return (-1);
 	while (total < n)
 	{
-		if (file->fpos == -1 || file->fpos == file->lread)
-			if (file->fpos > 0 && file->lread < file->cap)
-			{
-				file->fpos = -1;
-				return (total);
-			}
-			else if (!f_feedf(file))
-				return (total);
+		if (check_buf(file))
+			return (total);
 		if (file->lread == -1)
 			return (-1);
 		csize = f_min(n, file->lread - file->fpos);
