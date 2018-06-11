79676f38916ffd63fadf4b348bda52c0251aea5b
Werner Seegers <wseegers.mauws@gmail.com>
2018-06-11 22:23:28 +0200

    added null handling to %s

--- f_print/pf_handle_str.c
+++ f_print/pf_handle_str.c
@@ -6,7 +6,7 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 22:00:06 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/10 13:38:01 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/11 22:23:08 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 
 int		pf_handle_str(char *buf, t_tag *tag, va_list ap, size_t n)
 {
+	char *str;
+
 	s_tag_get_width(tag, ap);
 	s_tag_get_prec(tag, ap);
 	if (tag->spec == 'S' || (tag->spec == 's' && tag->mem_size >= sizeof(int)))
@@ -25,7 +27,13 @@ int		pf_handle_str(char *buf, t_tag *tag, va_list ap, size_t n)
 							(tag->spec == 'c' && tag->mem_size >= sizeof(int)))
 		pf_wctostr(buf, va_arg(ap, int));
 	else if (tag->spec == 's')
+	{
+		if (!(str = va_arg(ap, char*)))
+		{
+			str = "(null)";
+		}
 		f_strncpy(buf, va_arg(ap, char*), n);
+	}
 	else if (tag->spec == 'c')
 	{
 		buf[0] = va_arg(ap, int) & 0xff;
