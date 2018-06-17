18e5610681a72694e49b01e777b39c3424fdbe51
Werner Seegers <wseegers.mauws@gmail.com>
2018-06-17 23:52:58 +0200

    fixxed point to double pointer

--- f_memory/f_swapptr.c
+++ f_memory/f_swapptr.c
@@ -6,15 +6,15 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/17 23:01:54 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/17 23:03:18 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/17 23:49:57 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	f_swapptr(void *p1, void *p2)
+void	f_swapptr(void **p1, void **p2)
 {
 	void *temp;
 
-	temp = p1;
-	p1 = p2;
-	p2 = temp;
+	temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
 }
