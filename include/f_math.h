4c02cbaaf8571fcb7240b5a96b836bdb9a76154d
Werner SEEGERS <wseegers@c1r2s2.wethinkcode.co.za>
2018-07-16 13:22:16 +0200

    added maxmin macro

--- include/f_math.h
+++ include/f_math.h
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   f_math.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
+/*   By: wseegers <wseegers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 11:53:54 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/07 03:29:00 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/07/16 11:35:46 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef F_MATH_H
 # define F_MATH_H
+
+# define MIN(n1, n2)    (n1 < n2) ? n1 : n2
+# define MAX(n1, n2)    (n1 > n2) ? n1 : n2
 
 long long	f_abs(long long nbr);
 long		f_min(long base, long test);
